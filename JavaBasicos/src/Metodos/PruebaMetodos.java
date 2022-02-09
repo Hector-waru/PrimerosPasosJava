13a13a1b9a833ff23ca970caab22f36341c5e529
Hector Garcia Ortiz <Hector.Garcia.Ortiz@gmail.com>
2022-02-09 15:10:15 -0600

    # WARNING: head commit changed in the meantime
    Ejercicios 31 de enero - ciclos

--- JavaBasicos/src/Metodos/PruebaMetodos.java
+++ JavaBasicos/src/Metodos/PruebaMetodos.java
@@ -2,9 +2,10 @@ package Metodos;
 
 public class PruebaMetodos {
 	 public static void main(String[] args) {
-		      //un estatic funciona sin objetos 
+		 //un estatic funciona sin objetos 
 		 Metodos.imprimirNombre("Hector");
-			//Crear un nuevo objeto que va a contener los metodos y atributos
+			
+		 //Crear un nuevo objeto que va a contener los metodos y atributos
 			   Metodos metodoPrueba = new Metodos();
 			   metodoPrueba.a=10;
 			   metodoPrueba.b=26;
